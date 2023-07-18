00d85227d48667f23a60a49b645568ea1b4f5440
Ammar Abdo <90091361+AmmarAbdoh@users.noreply.github.com>
2023-07-18 18:39:51 +0300

    Initial commit

---
+++ src/AppIcon.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function AppIcon({}) {
+  return (
+    <div className="d-none d-md-block">
+      <div className="d-flex justify-content-center">
+        <img src="/public/icon.svg" className="icon img-fluid" alt="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default AppIcon;
---
+++ src/AppTitle.jsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+function AppTitle({}) {
+  return (
+    <div className="d-flex justify-content-center">
+      <h1
+        style={{
+          color: "black",
+        }}
+      >
+        Video Searching App
+      </h1>
+    </div>
+  );
+}
+
+export default AppTitle;
